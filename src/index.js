fa11bebddeff31435da84c128f4fce3b4d4d77b2
taher27 <mandapwalataher@gmail.com>
2019-06-18 17:43:12 +0600

    initial Commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Remarkable from 'remarkable';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class MarkDownEditor extends React.Component {
+    constructor(props) {
+        super(props);
+            this.handleChange = this.handleChange.bind(this);
+            this.state = {
+                value: 'Try Something...'
+            };
+        }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+        handleChange(e) {
+            this.setState({
+                value: e.target.value
+            });
+        }
+        
+        getRawMarkup() {
+            const md = new Remarkable();
+            return {__html: md.render(this.state.value)};
+        }
+
+        render() {
+            return (
+                <div className="container">
+                    <div className="input">
+                        <h3>Input goes here</h3>
+                        <textarea
+                            className="input-text"
+                            onChange={this.handleChange}
+                            defaultValue={this.state.value}
+                        />
+                    </div>
+                    <div className="output">
+                        <h3>MarkDown</h3>
+                        <div 
+                            dangerouslySetInnerHTML={this.getRawMarkup()}
+                            className="output-text"
+                        >
+                        </div>
+                    </div>
+                </div>
+            )
+        }
+    }
+ReactDOM.render(
+    <MarkDownEditor />,
+    document.getElementById('root')
+);
